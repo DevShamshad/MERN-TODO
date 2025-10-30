abe0d07264e9176b55648c6cd75df7ffbf04e7a5
DevShamshad <shamshadansari2929@gmail.com>
2025-10-30 19:00:52 +0530

    Made changes for alignment issue fix

--- mern-todo/frontend/src/pages/Login.jsx
+++ mern-todo/frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-// import "./Auth.css";
+import "./Login.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
--- mern-todo/frontend/src/pages/Register.jsx
+++ mern-todo/frontend/src/pages/Register.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-// import "./Auth.css";
+import "./Register.css";
 
 const Register = () => {
   const [name, setName] = useState("");
